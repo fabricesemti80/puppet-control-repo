08ebcc6a5e88ef141fa5b8258a31e05c829556e3
Fabrice <fabrice@fabricesemti.com>
2022-11-20 17:52:23 +0000

    fix message

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,6 +1,6 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
   class { 'motd':
-    content => "Hello! You are in ${trusted['extensions']['pp_datacenter']}.\nWe are all puppets here!\n",
+    content => "We are all puppets here!\n",
   }
 }
