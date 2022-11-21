0f21c882a8f5f3655ab091d1169f98e6b62d92e3
Fabrice <fabrice@fabricesemti.com>
2022-11-21 10:05:14 +0000

    fix: ✨ move the motd

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,6 +1,6 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
-  class { 'motd':
-    content => "We are all puppets here!\n",
-  }
+  # class { 'motd':
+  #   content => "We are all puppets here!\n",
+  # }
 }
