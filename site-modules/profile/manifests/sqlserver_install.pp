6d99b2af0a6dd9faf7f71d23d828a3b5575d95bc
Fabrice <fabrice@fabricesemti.com>
2022-11-22 15:53:56 +0000

    fix: ⚡️ simplify SQL install

--- site-modules/profile/manifests/sqlserver_install.pp
+++ site-modules/profile/manifests/sqlserver_install.pp
@@ -1,13 +1,26 @@
 # This role would be made of all the profiles that need to be included to make a database server work
-class profile::sqlserver_install {
-  $sourceloc = 'F:/'
-
-# Install a SQL Server default instance
-  sqlserver_instance { 'MSSQLSERVER':
-    source                => $sourceloc,
-    features              => ['SQLEngine'],
-    sql_sysadmin_accounts => [$facts['identity']['user']],
-    install_switches      => {
+class profile::sqlserver_install (
+  $instance_name = 'MSSQLSERVER',
+  $windows_user = 'dbuser',
+  $sapwd = 'sapassword123$',
+  $windows_pwd = 'winpassword123$',
+  $source_letter = 'F:/',
+) {
+  user { 'windows_user':
+    ensure     => present,
+    name       => $windows_user,
+    groups     => ['Users','Administrators'],
+    password   => $windows_pwd,
+    managehome => true,
+  }
+  sqlserver_instance { $instance_name:
+    source                 => $source_letter,
+    security_mode          => 'SQL',
+    sa_pwd                 => $sapwd,
+    features               => ['SQL'],
+    sql_sysadmin_accounts  => [$windows_user],
+    windows_feature_source => 'C:\Windows\WinSxS',
+    install_switches       => {
       'TCPENABLED'          => 1,
       'SQLBACKUPDIR'        => 'C:\\MSSQLSERVER\\backupdir',
       'SQLTEMPDBDIR'        => 'C:\\MSSQLSERVER\\tempdbdir',
@@ -17,43 +30,8 @@ class profile::sqlserver_install {
       'INSTALLSHAREDWOWDIR' => 'C:\\Program Files (x86)\\Microsoft SQL Server',
     },
   }
-
-# Resource to connect to the DB instance
-  sqlserver::config { 'MSSQLSERVER':
-    admin_login_type => 'WINDOWS_LOGIN',
-  }
-
-# Enforce SQL Server Administrators
-  $local_dba_group_name = 'DB Administrators'
-  $local_dba_group_netbios_name = "${facts['networking']['hostname']}\\DB Administrators"
-
-  group { $local_dba_group_name:
-    ensure => present,
-  }
-
-  -> sqlserver::login { $local_dba_group_netbios_name :
-    login_type  => 'WINDOWS_LOGIN',
-  }
-
-  -> sqlserver::role { 'sysadmin':
-    ensure   => 'present',
-    instance => 'MSSQLSERVER',
-    type     => 'SERVER',
-    members  => [$local_dba_group_netbios_name, $facts['identity']['user']],
-  }
-
-# Enforce memory consumption
-  sqlserver_tsql { 'check advanced sp_configure':
-    command  => 'EXEC sp_configure \'show advanced option\', \'1\'; RECONFIGURE;',
-    onlyif   => 'sp_configure @configname=\'max server memory (MB)\'',
-    instance => 'MSSQLSERVER',
-  }
-
-  -> sqlserver::sp_configure { 'MSSQLSERVER-max memory':
-    config_name => 'max server memory (MB)',
-    instance    => 'MSSQLSERVER',
-    reconfigure => true,
-    restart     => true,
-    value       => 2048,
+  sqlserver_features { 'Generic Features':
+    source   => $source_letter,
+    features => ['SQLEngine',  'SSMS'],
   }
 }
