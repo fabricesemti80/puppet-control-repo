f512392c51d7157e2e311c37b573af70b3ad60d0
Fabrice <fabrice@fabricesemti.com>
2022-11-22 15:10:15 +0000

    feat: 🎨 sql server install attempt 2

--- site-modules/role/manifests/database_server.pp
+++ site-modules/role/manifests/database_server.pp
@@ -1,21 +1,23 @@
 # This role would be made of all the profiles that need to be included to make a database server work
 # All roles should include the base profile
 class role::database_server {
+  (
+    $instance_name = 'MSSQLSERVER',
+    $windows_user = $facts['cmp_database_user'],
+    $sapwd = $facts['cmp_db_password'],
+    $windows_pwd = $facts['cmp_db_password'],
+    $source_letter = 'Z:/',
+  )
   include profile::base
+  include profile::sqlserver_installation
   class { 'motd':
     content => "This is a Puppet-managed database server!\n",
-  }
-
-  sqlserver_instance { 'MSSQLSERVER':
-    features              => ['SQL'],
-    source                => 'F:/',
-    sql_sysadmin_accounts => ['myuser'],
   }
 
   # # install management studio
   # package { 'sql-server-management-studio':
   #   ensure   => '15.0.18424.0',
   #   provider => 'chocolatey',
-  #   source   => 'https://community.chocolatey.org/api/v2/',
+  #   source   => 'https://chocolatey.org/packages',
   # }
 }
