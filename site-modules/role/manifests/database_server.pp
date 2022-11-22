314f9201f0cc5088c31a54e3183803834212e918
Fabrice <fabrice@fabricesemti.com>
2022-11-22 15:14:07 +0000

    fix: 🐛 those parameters should have been removed - i swear

--- site-modules/role/manifests/database_server.pp
+++ site-modules/role/manifests/database_server.pp
@@ -1,13 +1,6 @@
 # This role would be made of all the profiles that need to be included to make a database server work
 # All roles should include the base profile
 class role::database_server {
-  (
-    $instance_name = 'MSSQLSERVER',
-    $windows_user = $facts['cmp_database_user'],
-    $sapwd = $facts['cmp_db_password'],
-    $windows_pwd = $facts['cmp_db_password'],
-    $source_letter = 'Z:/',
-  )
   include profile::base
   include profile::sqlserver_installation
   class { 'motd':
