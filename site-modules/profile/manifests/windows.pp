d8cf4461925febb5db77a2b3044c59a30861b397
Fabrice <fabrice@fabricesemti.com>
2022-11-23 11:31:20 +0000

    feat: 💄 enable long file path support in base profile
    - https://forge.puppet.com/modules/dsc/updateservicesdsc/readme#usage

--- site-modules/profile/manifests/windows.pp
+++ site-modules/profile/manifests/windows.pp
@@ -1,12 +1,23 @@
 # The Windows profile should be applied to all Windows nodes
 class profile::windows {
+  ## test file - to demonstrate that Puppet is working
   file { 'c:/puppet-was-here.txt':
     ensure => 'file',
     group  => 'Administrators',
   }
+  ## ensure time service is running - demonstration purposed mainly
   service { 'w32time':
     ensure => 'running',
   }
+  # ## fix PS gallery
   # include profile::fix_psgallery
+  ## patch managemenet
   include profile::patch_mgmt_win
+  ## support for long file paths
+  registry_value { 'HKLM\System\CurrentControlSet\Control\FileSystem\LongPathsEnabled':
+    ensure   => 'present',
+    data     => [1],
+    provider => 'registry',
+    type     => 'dword',
+  }
 }
