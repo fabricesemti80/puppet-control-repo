82ed25131b048bef4d0cddee85ca903e51c78cc3
fabrice@dev-force-1 <fabrice@fabricesemti>
2022-12-16 12:33:39 +0000

    correct trusted -> Trusted

--- site-modules/profile/manifests/windows.pp
+++ site-modules/profile/manifests/windows.pp
@@ -38,6 +38,6 @@ class profile::windows {
   dsc_psrepository { 'Trust public gallery':
     dsc_name               => 'PSGallery',
     dsc_ensure             => 'Present',
-    dsc_installationpolicy => trusted,
+    dsc_installationpolicy => 'Trusted',
   }
 }
